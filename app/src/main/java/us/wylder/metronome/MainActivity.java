014d14d0e3a9aad1da7fcab5ecfccdf6ef02c630
Matthew Wylder <matt@wylder.us>
2015-07-17 17:35:40 -0500

    initial commit

---
+++ app/src/main/java/us/wylder/metronome/MainActivity.java
@@ -0,0 +1,46 @@
+package us.wylder.metronome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private SeekBar seekBpm;
+    private TextView textBpm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBpm = (SeekBar) findViewById(R.id.seek_bpm);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
